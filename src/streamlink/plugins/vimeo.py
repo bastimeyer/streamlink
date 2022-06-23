6db0250e1495478b1e69a430ae3d98eb91e91788
bastimeyer <mail@bastimeyer.de>
2022-06-23 16:22:48 +0200

    plugins.vimeo: fix minor issues
    - Raise correct PluginError/StreamError on api.set_expiry_time()
    - Remove redundant code paths
    - Rename EXPIRY_TIME_LIMIT class attribute constant
    - Move and fix config schema definition
    - Use f-strings and fix URL concatenation

--- src/streamlink/plugins/vimeo.py
+++ src/streamlink/plugins/vimeo.py
@@ -12,7 +12,7 @@ from time import time
 from urllib.parse import urlparse
 
 from streamlink.exceptions import StreamError
-from streamlink.plugin import Plugin, PluginArgument, PluginArguments, pluginmatcher
+from streamlink.plugin import Plugin, PluginArgument, PluginArguments, PluginError, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.dash import DASHStream
 from streamlink.stream.ffmpegmux import MuxedStream
@@ -33,17 +33,23 @@ class VimeoHLSStream(HLSStream):
     def __init__(self, session_, url, **args):
         self.session = session_
         self._url = url
-        self.api = args.pop("api")
-        self._parsed_url = urlparse(self._url)
+        self.api: VimeoAPI = args.pop("api")
+        self._parsed_url = urlparse(url)
         self._path_parts = self._parsed_url.path.split("/")
         self._need_update = False
-        self.api.set_expiry_time(self._path_parts[1])
-        super().__init__(self.session, self._url, **args)
+        try:
+            self.api.set_expiry_time(self._path_parts[1])
+        except ValueError as err:
+            raise PluginError(err)
+        super().__init__(session_, url, **args)
 
     @property
     def url(self):
         if self._need_update and not self.api.updating:
-            self.api.set_expiry_time(self.api.auth_data)
+            try:
+                self.api.set_expiry_time(self.api.auth_data)
+            except ValueError as err:
+                raise StreamError(err)
             self._path_parts[1] = self.api.auth_data
             self._url = self._parsed_url._replace(path="/".join(self._path_parts)).geturl()
             log.debug("Reloaded Vimeo HLS URL")
@@ -51,38 +57,19 @@ class VimeoHLSStream(HLSStream):
 
         time_now = time()
         if time_now > self.api.expiry_time:
-            if self.api.updating or time_now - self.api.last_updated < VimeoAPI._expiry_time_limit:
-                self._need_update = True
-            else:
+            if not self.api.updating and time_now >= self.api.last_updated + self.api.EXPIRY_TIME_LIMIT:
                 log.debug("Reloading Vimeo auth data")
                 self.api.reload_auth_data(self._path_parts[1])
-                self._need_update = True
+            self._need_update = True
 
         return self._url
 
 
 class VimeoAPI:
-    _expiry_time_limit = 60
+    EXPIRY_TIME_LIMIT = 60
+
     _expiry_re1 = re.compile(r"^exp=(\d+)~")
     _expiry_re2 = re.compile(r"^(\d+)-")
-
-    _config_schema = validate.Schema(
-        validate.parse_json(),
-        {
-            "request": {
-                "files": {
-                    validate.optional("dash"): {"cdns": {str: {"url": validate.any(None, validate.url())}}},
-                    validate.optional("hls"): {"cdns": {str: {"url": validate.any(None, validate.url())}}},
-                    validate.optional("progressive"): validate.all(
-                        [{"url": validate.url(), "quality": str}],
-                    ),
-                },
-                validate.optional("text_tracks"): validate.all(
-                    [{"url": str, "lang": str}],
-                ),
-            },
-        },
-    )
 
     def __init__(self, session, url):
         self.session = session
@@ -92,43 +79,60 @@ class VimeoAPI:
         self.last_updated = time()
         self.updating = False
 
+        schema_cdns = {"cdns": {str: {"url": validate.any(None, validate.url())}}}
+        self._schema_config = validate.Schema(
+            validate.parse_json(),
+            {
+                "request": {
+                    "files": {
+                        validate.optional("dash"): schema_cdns,
+                        validate.optional("hls"): schema_cdns,
+                        validate.optional("progressive"): [{"url": validate.url(), "quality": str}],
+                    },
+                    validate.optional("text_tracks"): [{"url": str, "lang": str}],
+                },
+            },
+            validate.get("request"),
+        )
+
     def get_player_data(self):
         return self.session.http.get(self.url, schema=validate.Schema(
             validate.transform(re.compile(r"var\s+config\s*=\s*({.+?})\s*;").search),
-            validate.any(None, validate.Schema(validate.get(1), self._config_schema)),
+            validate.any(None, validate.all(
+                validate.get(1),
+                self._schema_config,
+            )),
         ))
 
     def get_api_url(self):
         return self.session.http.get(self.url, schema=validate.Schema(
             validate.transform(re.compile(r'(?:"config_url"|\bdata-config-url)\s*[:=]\s*(".+?")').search),
-            validate.any(
-                None,
-                validate.Schema(
-                    validate.get(1),
-                    validate.parse_json(),
-                    validate.transform(html_unescape),
-                    validate.url(),
-                ),
-            ),
+            validate.any(None, validate.all(
+                validate.get(1),
+                validate.parse_json(),
+                validate.transform(html_unescape),
+                validate.url(),
+            )),
         ))
 
     def get_config_data(self, api_url):
-        return self.session.http.get(api_url, schema=self._config_schema)
+        return self.session.http.get(api_url, schema=self._schema_config)
 
     def get_data(self):
         if "player.vimeo.com" in self.url:
             return self.get_player_data()
-        else:
-            api_url = self.get_api_url()
-            if not api_url:
-                return
-            return self.get_config_data(api_url)
+
+        api_url = self.get_api_url()
+        if not api_url:
+            return
+
+        return self.get_config_data(api_url)
 
     def set_expiry_time(self, path):
         m = self._expiry_re1.search(path) or self._expiry_re2.search(path)
         if not m:
-            raise StreamError("expiry value not found in URL")
-        self.expiry_time = int(m.group(1)) - self._expiry_time_limit
+            raise ValueError("expiry value not found in URL")
+        self.expiry_time = int(m.group(1)) - self.EXPIRY_TIME_LIMIT
 
     def reload_auth_data(self, auth_part):
         self.updating = True
@@ -136,30 +140,30 @@ class VimeoAPI:
 
         data = self.get_data()
         if not data:
-            raise StreamError("no video data found")
-        videos = data["request"]["files"]
-        if "hls" not in videos:
-            raise StreamError("hls key not found in video data")
+            raise StreamError("No video data found")
 
-        new_auth = None
-        for _, video_data in videos["hls"]["cdns"].items():
-            url = video_data.get("url")
+        videos = data["files"]
+        if "hls" not in videos:
+            raise StreamError("HLS key not found in video data")
+
+        for video_data in videos["hls"]["cdns"].values():
+            url: str = video_data.get("url")
+            if not url:
+                continue
             res = self.session.http.get(url)
             if res.history:
                 url = res.url
 
             parsed_url = urlparse(url)
             path_parts = parsed_url.path.split("/")
-            if self._expiry_re1.search(path_parts[1]) and self._expiry_re1.search(auth_part):
-                new_auth = path_parts[1]
-            elif self._expiry_re2.search(path_parts[1]) and self._expiry_re2.search(auth_part):
-                new_auth = path_parts[1]
-
-            if new_auth:
-                self.auth_data = new_auth
+            if (
+                self._expiry_re1.search(path_parts[1]) and self._expiry_re1.search(auth_part)
+                or self._expiry_re2.search(path_parts[1]) and self._expiry_re2.search(auth_part)
+            ):
+                self.auth_data = path_parts[1]
                 break
-        if not new_auth:
-            raise StreamError("failed to get new auth data from URL")
+        else:
+            raise StreamError("Failed to get new auth data from URL")
 
         self.updating = False
 
@@ -173,36 +177,41 @@ class Vimeo(Plugin):
     )
 
     def _get_streams(self):
-        self.api = VimeoAPI(self.session, self.url)
-        data = self.api.get_data()
+        api = VimeoAPI(self.session, self.url)
+        data = api.get_data()
         if not data:
             return
 
-        videos = data["request"]["files"]
+        videos = data["files"]
         streams = []
 
         for stream_type in ("hls", "dash"):
             if stream_type not in videos:
                 continue
-            for _, video_data in videos[stream_type]["cdns"].items():
-                log.trace("{0!r}".format(video_data))
+            for video_data in videos[stream_type]["cdns"].values():
+                log.trace(f"{video_data!r}")
                 url = video_data.get("url")
                 if not url:
-                    log.error("This video requires a logged in session to view it")
+                    log.error("This video requires a logged-in session to view it")
                     return
+
                 if stream_type == "hls":
-                    for stream in VimeoHLSStream.parse_variant_playlist(self.session, url, api=self.api).items():
+                    for stream in VimeoHLSStream.parse_variant_playlist(self.session, url, api=api).items():
                         streams.append(stream)
                 elif stream_type == "dash":
                     p = urlparse(url)
                     if p.path.endswith("dash.mpd"):
                         # LIVE
-                        url = self.session.http.get(url).json()["url"]
+                        url = self.session.http.get(url, schema=validate.Schema(
+                            validate.parse_json(),
+                            {"url": validate.url()},
+                            validate.get("url"),
+                        ))
                     elif p.path.endswith("master.json"):
                         # VOD
                         url = url.replace("master.json", "master.mpd")
                     else:
-                        log.error("Unsupported DASH path: {0}".format(p.path))
+                        log.error(f"Unsupported DASH path: {p.path}")
                         continue
 
                     for stream in DASHStream.parse_manifest(self.session, url).items():
@@ -211,16 +220,17 @@ class Vimeo(Plugin):
         for stream in videos.get("progressive", []):
             streams.append((stream["quality"], HTTPStream(self.session, stream["url"])))
 
-        if self.get_option("mux_subtitles") and data["request"].get("text_tracks"):
-            substreams = {
-                s["lang"]: HTTPStream(self.session, "https://vimeo.com" + s["url"])
-                for s in data["request"]["text_tracks"]
-            }
+        if self.get_option("mux_subtitles") and data.get("text_tracks"):
+            substreams = {}
+            for text_track in data.get("text_tracks"):
+                text_lang: str = text_track["lang"]
+                text_url: str = text_track["url"]
+                text_url = urlparse(text_url)._replace(scheme="https", netloc="vimeo.com").geturl()
+                substreams[text_lang] = HTTPStream(self.session, text_url)
             for quality, stream in streams:
                 yield quality, MuxedStream(self.session, stream, subtitles=substreams)
         else:
-            for stream in streams:
-                yield stream
+            yield from streams
 
 
 __plugin__ = Vimeo
